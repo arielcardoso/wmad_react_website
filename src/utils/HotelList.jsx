7ab5a2f915f3582d2010716059fdc512a9073c93
Ariel Cardoso <arielvix@gmail.com>
2020-11-30 17:17:56 -0300

    Add service list

---
+++ src/utils/HotelList.jsx
@@ -0,0 +1,77 @@
+import img_01 from '../assets/images/services/hotel/01.jpg';
+import img_02 from '../assets/images/services/hotel/02.jpg';
+import img_03 from '../assets/images/services/hotel/03.jpg';
+import img_04 from '../assets/images/services/hotel/04.jpg';
+import img_05 from '../assets/images/services/hotel/05.jpg';
+import img_06 from '../assets/images/services/hotel/06.jpg';
+import img_07 from '../assets/images/services/hotel/07.jpg';
+import img_08 from '../assets/images/services/hotel/08.jpg';
+import img_09 from '../assets/images/services/hotel/09.jpg';
+
+const HotelList = [
+    {
+        id: 1,
+        name: "Hotel Name", 
+        description: "Lorem Ipsum", 
+        image: img_01,
+        ratings: 4.9,
+    },
+    {
+        id: 2,
+        name: "Hotel Name", 
+        description: "Lorem Ipsum", 
+        image: img_02,
+        ratings: 4.1,
+    },
+    {
+        id: 3,
+        name: "Hotel Name", 
+        description: "Lorem Ipsum", 
+        image: img_03,
+        ratings: 5,
+    },
+    {
+        id: 4,
+        name: "Hotel Name", 
+        description: "Lorem Ipsum", 
+        image: img_04,
+        ratings: 4.8,
+    },
+    {
+        id: 5,
+        name: "Hotel Name", 
+        description: "Lorem Ipsum", 
+        image: img_05,
+        ratings: 4.2,
+    },
+    {
+        id: 6,
+        name: "Hotel Name", 
+        description: "Lorem Ipsum", 
+        image: img_06,
+        ratings: 5,
+    },
+    {
+        id: 7,
+        name: "Hotel Name", 
+        description: "Lorem Ipsum", 
+        image: img_07,
+        ratings: 4.2,
+    },
+    {
+        id: 8,
+        name: "Hotel Name", 
+        description: "Lorem Ipsum", 
+        image: img_08,
+        ratings: 4.8,
+    },
+    {
+        id: 9,
+        name: "Hotel Name", 
+        description: "Lorem Ipsum", 
+        image: img_09,
+        ratings: 3.2,
+    }
+];
+
+export default HotelList;
---
+++ src/utils/RestaurantList.jsx
@@ -0,0 +1,77 @@
+import img_01 from '../assets/images/services/restaurant/01.jpg';
+import img_02 from '../assets/images/services/restaurant/02.jpg';
+import img_03 from '../assets/images/services/restaurant/03.jpg';
+import img_04 from '../assets/images/services/restaurant/04.jpg';
+import img_05 from '../assets/images/services/restaurant/05.jpg';
+import img_06 from '../assets/images/services/restaurant/06.jpg';
+import img_07 from '../assets/images/services/restaurant/07.jpg';
+import img_08 from '../assets/images/services/restaurant/08.jpg';
+import img_09 from '../assets/images/services/restaurant/09.jpg';
+
+const RestaurantList = [
+    {
+        id: 1,
+        name: "Restaurant Name", 
+        description: "Lorem Ipsum", 
+        image: img_01,
+        ratings: 4.2,
+    },
+    {
+        id: 2,
+        name: "Restaurant Name", 
+        description: "Lorem Ipsum", 
+        image: img_02,
+        ratings: 4.2,
+    },
+    {
+        id: 3,
+        name: "Restaurant Name", 
+        description: "Lorem Ipsum", 
+        image: img_03,
+        ratings: 4.2,
+    },
+    {
+        id: 4,
+        name: "Restaurant Name", 
+        description: "Lorem Ipsum", 
+        image: img_04,
+        ratings: 4.2,
+    },
+    {
+        id: 5,
+        name: "Restaurant Name", 
+        description: "Lorem Ipsum", 
+        image: img_05,
+        ratings: 4.2,
+    },
+    {
+        id: 6,
+        name: "Restaurant Name", 
+        description: "Lorem Ipsum", 
+        image: img_06,
+        ratings: 4.2,
+    },
+    {
+        id: 7,
+        name: "Restaurant Name", 
+        description: "Lorem Ipsum", 
+        image: img_07,
+        ratings: 4.2,
+    },
+    {
+        id: 8,
+        name: "Restaurant Name", 
+        description: "Lorem Ipsum", 
+        image: img_08,
+        ratings: 4.2,
+    },
+    {
+        id: 9,
+        name: "Restaurant Name", 
+        description: "Lorem Ipsum", 
+        image: img_09,
+        ratings: 4.2,
+    }
+];
+
+export default RestaurantList;
