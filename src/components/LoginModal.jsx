843e347f59094ee2223a1088b4c4f3c4d8eeef73
Ariel Cardoso <arielvix@gmail.com>
2020-12-02 00:49:23 -0300

    Login form using dialog

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -1,29 +1,9 @@
 import React, { forwardRef, useImperativeHandle } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography, TextField, Button } from '@material-ui/core';
-
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
-
-const useStyles = makeStyles((theme) => ({
-  modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  paper: {
-    backgroundColor: '#fff',
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: '2rem',
-    width: '90%',
-    maxWidth: '400px',
-  },
-}));
+import { TextField, Button, Dialog, DialogActions, DialogContent, IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
 
 const LoginModal = forwardRef((props, ref) => {
-  const classes = useStyles();
 
   const [open, setOpen] = React.useState(false);
 
@@ -40,32 +20,35 @@ const LoginModal = forwardRef((props, ref) => {
      }),
   )
 
+  const CustomDialogTitle = ((props) => {
+    const { children, onClose } = props;
+    return (
+      <MuiDialogTitle disableTypography className='custom-dialog-title' >
+        <div className="title">{children}</div>
+        {onClose ? (
+          <IconButton aria-label="close" className='close-button' onClick={onClose}>
+            <CloseIcon />
+          </IconButton>
+        ) : null}
+      </MuiDialogTitle>
+    );
+  });
+
   return (
     <div>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        className={classes.modal}
-        open={open}
-        onClose={handleOpen}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <div className={classes.paper} >
-            <Typography variant="h4" component="h4" gutterBottom>LOGIN</Typography>
-            <form action="post" >
-              <TextField id="loginUsername" label="Username" fullWidth />
-              <TextField id="loginPassword" label="Password" fullWidth />
-              <a href="/" style={{margin:'-10px 0 10px', display:'block', fontSize:'0.8rem', textAlign:'right' }} >Forgot password?</a>
-              <Button fullWidth type="submit" variant="contained" color="primary" >LOGIN</Button>
-            </form>
-          </div>
-        </Fade>
-      </Modal>
+      <Dialog open={open} onClose={handleOpen} aria-labelledby="form-dialog">
+        <form>
+          <CustomDialogTitle onClose={handleOpen}>LOGIN</CustomDialogTitle>
+          <DialogContent>
+            <TextField id="loginUsername" label="Username" fullWidth />
+            <TextField id="loginPassword" label="Password" fullWidth />
+            <a href="/" className="forgot-password">Forgot password?</a>
+          </DialogContent>
+          <DialogActions>
+            <Button fullWidth onClick={handleOpen} type="submit" variant="contained" color="primary">LOGIN</Button>
+          </DialogActions>
+        </form>
+      </Dialog>
     </div>
   );
 });
